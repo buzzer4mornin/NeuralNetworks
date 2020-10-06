61e6de4d8c9c4cccdc603807d105baf5804bb354
ahmadliaydin <aydin.ahmadli@merck.com>
2020-10-06 16:29:47 +0200

    First setup: adding words

---
+++ mainfile.py
@@ -0,0 +1,22 @@
+#Generating the training data
+#Taking a random list of 20 words with this website : https://www.randomlists.com/random-words?dup=false&qty=20
+word_list=['nice',
+'toothpaste',
+'halting',
+'deafening',
+'structure',
+'soak',
+'omniscient',
+'found',
+'authority',
+'crabby',
+'blush',
+'dreary',
+'childlike',
+'nippy',
+'roll',
+'highfalutin',
+'wing',
+'hole',
+'nonchalant',
+'plant']
