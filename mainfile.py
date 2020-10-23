aa22fe5e7e23adea245dcda15a69ba1dd1cdb647
ahmadliaydin <aydin.ahmadli@merck.com>
2020-10-23 22:15:01 +0200

    adding shuffle_letters()

--- mainfile.py
+++ mainfile.py
@@ -1,12 +1,31 @@
-#Generating the training data
-#Taking a random list of 20 words with this website : https://www.randomlists.com/random-words?dup=false&qty=20
+import random
 
-word_list=['nice', 'toothpaste', 'halting', 'deafening', 'structure', 'soak', 'omniscient', 'found', 'authority',
-        'crabby', 'blush', 'dreary', 'childlike', 'nippy', 'roll', 'highfalutin', 'wing', 'hole', 'nonchalant', 'plant']
+"""
+Generating the training data
+Taking a random list of 20 words with this website : https://www.randomlists.com/random-words?dup=false&qty=20
+"""
+
+word_list = ['nice', 'toothpaste', 'halting', 'deafening', 'structure', 'soak', 'omniscient', 'found', 'authority',
+             'crabby', 'blush', 'dreary', 'childlike', 'nippy', 'roll', 'highfalutin', 'wing', 'hole', 'nonchalant',
+             'plant']
+
+window_length = max([len(w) for w in word_list]) + 1
+reformat_to_window = lambda w: w + ' ' * (window_length - len(w))
+word_list_formated = [reformat_to_window(w) for w in word_list]
+
+"""
+Generating the appropriate train data : 
+- Balanced 
+- Negative closed to positive
+"""
 
 
-window_length=max([len(w) for w in word_list])+1
-reformat_to_window = lambda w : w+' '*(window_length-len(w))
-word_list_formated=[reformat_to_window(w) for w in word_list]
-
-
+def shuffle_letters(word, shuffle=5):
+    """ Recursive function that exchange two letters at each iteration (shuffle)"""
+    if type(word) is not list:
+        word = list(word)
+    if shuffle == 0:
+        return ''.join(word)  # word==shuffle_letters(word,shuffle=0) -> TRUE
+    i, j = random.randint(0, len(word) - 1), random.randint(0, len(word) - 1)
+    word[i], word[j] = word[j], word[i]
+    return shuffle_letters(word, shuffle - 1)
