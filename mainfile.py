79e405d43bc640b0e82b77ad39248895e007ae87
ahmadliaydin <aydin.ahmadli@merck.com>
2020-10-23 22:02:53 +0200

    input data preparation

--- mainfile.py
+++ mainfile.py
@@ -1,22 +1,12 @@
 #Generating the training data
 #Taking a random list of 20 words with this website : https://www.randomlists.com/random-words?dup=false&qty=20
-word_list=['nice',
-'toothpaste',
-'halting',
-'deafening',
-'structure',
-'soak',
-'omniscient',
-'found',
-'authority',
-'crabby',
-'blush',
-'dreary',
-'childlike',
-'nippy',
-'roll',
-'highfalutin',
-'wing',
-'hole',
-'nonchalant',
-'plant']
+
+word_list=['nice', 'toothpaste', 'halting', 'deafening', 'structure', 'soak', 'omniscient', 'found', 'authority',
+        'crabby', 'blush', 'dreary', 'childlike', 'nippy', 'roll', 'highfalutin', 'wing', 'hole', 'nonchalant', 'plant']
+
+
+window_length=max([len(w) for w in word_list])+1
+reformat_to_window = lambda w : w+' '*(window_length-len(w))
+word_list_formated=[reformat_to_window(w) for w in word_list]
+
+
