b9cc3a25c7d3a4690c5f9efb6fb98059260aaf59
ahmadliaydin <aydin.ahmadli@merck.com>
2020-10-24 16:37:27 +0200

    adding encode_window() and decode_window()

--- mainfile.py
+++ mainfile.py
@@ -36,18 +36,18 @@ train_data = [reformat_to_window(shuffle_letters(random.choice(word_list), rando
 
 # ========================================= Encode & Decode output ====================================================
 nn_output = np.zeros(len(word_list))
-
 """In order to train the model need a supervised train_data. Let's encode the output as a vector of binaries where 1 
 in i ith position if the model recognize the i ith word of the list. And declare as a non-sense if its recognize two 
 different words. """
 
 recognize = lambda w: np.array([1 if w == w_f else 0 for w_f in word_list_formated])
 train_y = [recognize(w) for w in train_data]
-#print(sum([sum(x) for x in train_y]) / len(train_data))  # proportion of correct words after shuffle
+# print(sum([sum(x) for x in train_y]) / len(train_data))  # proportion of correct words after shuffle
 
 
 decode_output = lambda o: 'Not recognize' if np.sum(o) >= 2 or np.sum(o) == 0 else word_list[o.tolist().index(1)]
 nn_output = train_y[0]
+#print(nn_output)
 #print(decode_output(nn_output))
 
 # ========================================= Encode & Decode input ====================================================
@@ -58,6 +58,26 @@ Encode the input look at this https://stackoverflow.com/questions/7396849/conver
 # Here is a little demonstration that we need 5 binaries to distinguish the 26 minus letter
 alpha = 'abcdefghijklmnopqrstuvwxyz'
 binaries = [bin(ord(letter_min))[-5:] for letter_min in list(alpha)]
-print(len(binaries), len(list(set(binaries)))) # suppress the doublon in the second list
+# print(len(binaries), len(list(set(binaries)))) # suppress the doublon in the second list
+
+nn_encode = np.zeros(window_length * 5)
 
 
+def encode_window(word=word_list_formated[0]):
+    global nn_encode
+    word_binaries = [bin(ord(l))[-5:] for l in list(word)]
+    word_binaries = ''.join(word_binaries)
+    word_binaries = [int(b) for b in list(word_binaries)]
+    return np.array(word_binaries)
+
+
+def decode_window(input_binaries_list):
+    word_binaries = input_binaries_list.tolist()
+    word_binaries = ''.join([str(b) for b in input_binaries_list])
+    word_binaries = ['011' + word_binaries[i:i + 5] for i in range(0, len(word_binaries), 5)]
+    # print(word_binaries)
+    return ''.join([chr(int(x, 2)) for x in word_binaries])
+
+
+nn_encode = encode_window(word=word_list_formated[0])
+decode_window(nn_encode)
