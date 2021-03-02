e11851cd049ad6668ef04717278997b8e85455dc
ahmadliaydin <aydin.ahmadli@merck.com>
2021-03-02 13:23:14 +0400

    final draft

--- word_recognition.py
+++ word_recognition.py
@@ -1,16 +1,16 @@
+import csv
 import random
 import numpy as np
 import lzma
 import pickle
 from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import mean_squared_error
 
 # ======================================================================================================================
 # =========================================== GENERATE POSITIVE WORDS ==================================================
 
-# Taking a random list of 20 words with this website : https://www.randomlists.com/random-words?dup=false&qty=20
-word_list = ['nice', 'toothpaste', 'halting', 'deafening', 'structure', 'soak', 'omniscient', 'found', 'authority',
-             'crabby', 'blush', 'dreary', 'childlike', 'nippy', 'roll', 'highfalutin', 'wing', 'hole', 'nonchalant',
-             'plant']
+# Taking a random list of 7 words with this website : https://www.randomlists.com/random-words?dup=false&qty=7
+word_list = ['nice', 'toothpaste', 'highfalutin', 'childlike', 'authority', 'halting', 'found']
 
 window_length = max([len(w) for w in word_list]) + 1
 reformat_to_window = lambda w: w + ' ' * (window_length - len(w))
@@ -37,12 +37,13 @@ train_data = [reformat_to_window(shuffle_letters(random.choice(word_list), rando
               for _ in range(0, size_of_data)]
 
 # In order to train the model, we also need train_target. So, lets encode the output as a vector of binaries where "1"
-# will be in ith position if the model recognize input word as ith word from original positive list of 20 words """
+# will be in ith position if the model recognize input word as ith word from original positive list of 7 words """
 recognize = lambda w: np.array([1 if w == w_f else 0 for w_f in word_list_formated])
 train_target = [recognize(w) for w in train_data]
 
 # Print ratio of positive words in training set
-print("Ratio of Positive data in training set:", sum([sum(x) for x in train_target]) / len(train_data))
+# print("Ratio of Positive data in training set:", sum([sum(x) for x in train_target]) / len(train_data))
+
 
 # ======================================================================================================================
 # ========================================= INPUT ENCODING & DECODING ==================================================
@@ -80,14 +81,21 @@ network = MLPClassifier(hidden_layer_sizes=(10, 10), activation='relu', solver='
 train_data_encoded = np.zeros(window_length * 5)
 train_data_encoded = [encode_window(word=train_data[i]) for i in range(len(train_data))]
 
-model = network.fit(train_data_encoded, train_target)
+with open('./input_representations.txt', 'w') as f:
+    for i in range(len(train_data)):
+        f.write(train_data[i])
+        f.write(",")
+        f.write(str(train_data_encoded[i]))
+        f.write("\n")
+
+# model = network.fit(train_data_encoded, train_target)
 
 # Save model
-with lzma.open("diacritization.model", "wb") as model_file:
-    pickle.dump(model, model_file)
+# with lzma.open("network.model", "wb") as model_file:
+#    pickle.dump(model, model_file)
 
 # Load model
-with lzma.open("diacritization.model", "rb") as model_file:
+with lzma.open("network.model", "rb") as model_file:
     model = pickle.load(model_file)
 
 # ======================================================================================================================
@@ -97,12 +105,13 @@ with lzma.open("diacritization.model", "rb") as model_file:
 decode_output = lambda o: 'Not recognize' if np.sum(o) >= 2 or np.sum(o) == 0 else word_list[o.tolist().index(1)]
 
 # Create Negative Words Test Set
-test_size = 10000
+test_size = 5000
 n_test = [shuffle_letters(random.choice(word_list), random.randint(1, 5)) \
           for _ in range(0, test_size)]
+n_test = [i for i in n_test if i not in word_list]
 
 # Create Negative + Positive Words Test Set
-test_size = 10000
+test_size = 5000
 n_p_test = [shuffle_letters(random.choice(word_list), random.randint(1, 5) * random.randint(0, 1)) \
             for _ in range(0, test_size)]
 
@@ -127,7 +136,7 @@ for w in n_p_test:
         if pred != w:
             n_p += 1
 
-# Calculate Accuracy on Original 20 words Positive Words set
+# Calculate Accuracy on Original 7 words Positive Words set
 o = 0
 for w in word_list:
     pred = decode_output(model.predict(encode_window(reformat_to_window(w)).reshape(1, -1))[0])
@@ -135,7 +144,37 @@ for w in word_list:
     if pred == w:
         o += 1
 
+# ======================================================================================================================
+print(
+    'Input                 Output                         Response               Error   Accuracy   Reliability')
+for i in range(len(n_test)):
+    output = np.zeros(shape=(7,))
+    pred = list(model.predict_proba(encode_window(reformat_to_window(n_test[i])).reshape(1, -1))[0])
+    for j in range(len(pred)):
+        pred[j] = float("{:.2f}".format(pred[j]))
+    acc = sum(output == pred) * 100 / len(output)
+    rely = sum(abs(output - pred) < 0.3) * 100 / len(output)
+    print(
+        f"{reformat_to_window(n_test[i])}  {output}   {pred}   {mean_squared_error(output, pred)}     {acc}        {rely}")
+
+# ======================================================================================================================
+print(
+    'Input                 Output                         Response               Error   Accuracy   Reliability')
+for i in range(len(n_p_test)):
+    if n_p_test[i] in word_list:
+        one = word_list.index(n_p_test[i])
+        output = np.zeros(shape=(7,))
+        output[one] = 1
+    else:
+        output = np.zeros(shape=(7,))
+    pred = list(model.predict_proba(encode_window(reformat_to_window(n_p_test[i])).reshape(1, -1))[0])
+    for j in range(len(pred)):
+        pred[j] = float("{:.2f}".format(pred[j]))
+    acc = sum(output == pred)*100/len(output)
+    rely = sum(abs(output - pred) < 0.3) * 100 / len(output)
+    print(f"{reformat_to_window(n_p_test[i])}  {output}   {pred}   {mean_squared_error(output, pred)}     {acc}        {rely}")
+
 # Print results
-print(f"Original Positive data accuracy: {o}/20 ({o / 20 * 100}%) \n"
+print(f"Original Positive data accuracy: {o}/7 ({o / 7 * 100}%) \n"
       f"Negative data accuracy: {test_size - n}/{test_size} ({(test_size - n) / test_size * 100}%) \n"
       f"Negative + Positive data accuracy: {test_size - n_p}/{test_size} ({(test_size - n_p) / test_size * 100}%)")
